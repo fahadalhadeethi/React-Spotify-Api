c2b915d8a26d7a431b128513226bb005312cbe4b
fahadalhadeethi <fahed.a.alhadithy@gmail.com>
2020-05-23 19:17:45 +0300

    change
    change

--- src/Search.js
+++ src/Search.js
@@ -3,19 +3,24 @@ import * as $ from "jquery";
 import { authEndpoint, clientId, redirectUri, scopes } from "./config";
 import {test,Usercontext,deletehash} from "./Auth"
 import Listitems from "./listitems";
-
+import { wait } from "@testing-library/react";
+import axios from 'axios';
 
 class Search extends Component {
  constructor() {
     super();
+   
+    this.timeout =  0
     if(window.location.hash!="")
     localStorage.setItem('session',test());
     this.state = {
         token: localStorage.getItem('session'),
         item: [],
-        lastsearch : ""
-       
-
+        lastsearch : "",
+        offset : 0,
+        loading : [],
+        next : 1
+     
             }
            deletehash()
          
@@ -25,28 +30,132 @@ class Search extends Component {
 }
 
   componentDidMount() {
-
-
-    if(localStorage.getItem("lastsearch")!="")
+      
+    window.addEventListener('scroll', this.handleScroll);
+    if(localStorage.getItem("lastsearch")!=""){
+      this.setState({
+        offset : 0,
+        next :1
+    
+    });
     this.getCurrentlyPlaying(this.state.token,localStorage.getItem("lastsearch"))
     
-
+    }
 
   }
+  componentWillUnmount(){
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+ 
+  async getCurrentlyPlaying(token,query) {
   
-
-  getCurrentlyPlaying(token,query) {
+   
+    if(this.state.next!==null){
+ try{
     if(query==""){
       this.setState({
           
         item: [],
-      
+        loading: []
       }); 
       return
     }
+   
+    const response = await fetch("https://api.spotify.com/v1/search?q="+query+"&type=artist&offset="+this.state.offset+"&limit=50",{method:"GET",
+     headers: new Headers ({"Authorization": "Bearer " + token,
+    "Content-Type": " Content-Type ",
+     "Accept": "application/json" 
+    })
+  })
+    const json = await response.json()
+   // console.log("req" ,json.artists.next)
+    if(json["error"]){
+      this.props.history.push('/login')
+      
+    }
+    this.setState({
+        offset : this.state.offset + 50,
+      item: this.state.item.concat(...json.artists.items),
+        next: json.artists.next
+    });
+  } catch (error) {
+    
+  }
+}else{
+  this.setState({
+    loading:[]
+  })
+}
+}
+
+
+   
+  //} catch(error) {
+    //  if(axios.isCancel(error)) {
+        // Handle if request was cancelled
+      //  console.log('Request canceled', error.message);
+    //  } else {
+        // Handle usual errors
+      //  console.log('Something went wrong: ', error.message)
+    //  }
+  //}
+   /* try {
+      
+      
+       if(query==""){
+        this.setState({
+            
+          item: [],
+        
+        }); 
+        return
+      }
+      const response = await fetch("https://api.spotify.com/v1/search?q="+query+"&type=artist",{method:"GET",
+       headers: new Headers ({"Authorization": "Bearer " + token,
+      "Content-Type": " Content-Type ",
+       "Accept": "application/json" 
+      })
+    })
+  
+      const json = await response.json()
+      this.setState({
+          
+        item: json.artists.items,
+      
+      });
+    } catch (error) {
+      
+    }*/
+   
+ 
+      
+    /*  if(query==""){
+        this.setState({
+            
+          item: [],
+        
+        }); 
+        return
+      }
+      const response = await fetch("https://api.spotify.com/v1/search?q="+query+"&type=artist",{method:"GET",
+       headers: new Headers ({"Authorization": "Bearer " + token,
+      "Content-Type": " Content-Type ",
+       "Accept": "application/json" 
+      })
+    })
+      const json = await response.json()
+      this.setState({
+          
+        item: json.artists.items,
+      
+      });
+    } catch (error) {
+      
+    }*/
+    
     
 
-    $.ajax({
+  /* $.ajax({
         
        
         url: "https://api.spotify.com/v1/search?q="+query+"&type=artist" ,
@@ -59,26 +168,65 @@ class Search extends Component {
       },
       success: data => {
         
-        this.setState({
+      this.setState({
           
           item: data.artists.items,
         
         });
-       console.log(this.state.item)
+     console.log("call",this.state.item)
       }
-    });
-  }
+    });*/
+   
+  
   changed = () =>{
   
   
   
-  localStorage.setItem('lastsearch',this.inputref.current.value);
+    localStorage.setItem('lastsearch',this.inputref.current.value);
     console.log("done",this.state.token)
-       this.getCurrentlyPlaying(this.state.token,this.inputref.current.value)
-          
+    this.setState({
+      offset :0,
+    item: [],
+    next : 1
+  });
+    if(this.timeout) clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      this.getCurrentlyPlaying(this.state.token,this.inputref.current.value)
+    }, 600);
+   
      
   }
-
+  handleScroll = () =>{
+    
+    
+    console.log(window.innerHeight + window.scrollY , "  ",document.body.offsetHeight )
+    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight){
+      if(this.timeout) clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      this.getCurrentlyPlaying(this.state.token,this.inputref.current.value)
+    }, 600);
+    
+      this.setState({
+        loading:[],
+       
+    
+    });
+  
+    }
+    if(this.state.next!==null){
+    this.setState({
+      loading:[<div   class="spinner-border text-dark" role="status"><span  class="sr-only">Loading...</span></div>],
+    
+  
+  });
+}
+    
+   //     this.getCurrentlyPlaying(this.state.token,localStorage.getItem("lastsearch"))
+   // }
+  //  this.loading.pop()
+  
+  
+}
   render() {
           if(localStorage.getItem("session")===""){
             return (
@@ -89,7 +237,7 @@ class Search extends Component {
                   <div class="input-group mb-3">
            
       
-     <input type="text" ref={this.inputref} onChange={this.changed} className="form-control" placeholder="Search for an Artist..." aria-label="Username" value={localStorage.getItem("lastsearch")}
+     <input type="text" ref={this.inputref} onChange={this.changed} className="form-control" placeholder="Search for an Artist..." aria-label="Username" 
      aria-describedby="basic-addon1" disabled/>
                    </div>
       <Listitems  list={this.state.item}/>
@@ -109,10 +257,12 @@ class Search extends Component {
                   <div class="input-group mb-3">
            
       
-     <input type="text" ref={this.inputref} onChange={this.changed} className="form-control" placeholder="Search for an Artist..." aria-label="Username" value={localStorage.getItem("lastsearch")}
+     <input type="text" ref={this.inputref} value={localStorage.getItem("lastsearch")}onChange={this.changed} className="form-control" placeholder="Search for an Artist..." aria-label="Username"
      aria-describedby="basic-addon1"/>
                    </div>
       <Listitems  list={this.state.item}/>
+      <div style={{textAlign:"center"}}> {this.state.loading}</div>
+     
               </div>
    
           
--- src/albumsApi.js
+++ src/albumsApi.js
@@ -12,7 +12,9 @@ class Albums extends React.Component {
     this.state = {
         token: localStorage.getItem('session'),
         item: [],
-       
+        loading : [],
+        offset:0,
+        next:1
     //  this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
             }
          
@@ -23,36 +25,89 @@ class Albums extends React.Component {
 }
   componentDidMount() {
     // Set token
+    window.addEventListener('scroll', this.handleScroll);
+    this.setState({
+      offset : 0,
+      next:1
+  
+  });
     this.getCurrentlyPlaying(this.state.token,this.props.match.params.id)
   //  let _token1 = hash.access_token;
 
   }
+  handleScroll = () =>{
+    
+    var scrollPos =  window.scrollY;
+    //console.log(window.innerHeight + window.scrollY , "  ",document.body.offsetHeight)
+    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight){
+      if(this.timeout) clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      this.getCurrentlyPlaying(this.state.token,this.props.match.params.id)
+    }, 600);
+    
+      this.setState({
+        loading:[],
+      
+    
+    });
+  
+    }
+    if(this.state.next!==null){
+    this.setState({
+      loading:[<div   class="spinner-border text-dark" role="status"><span  class="sr-only">Loading...</span></div>],
+    
+  
+  });
+}
+   //     this.getCurrentlyPlaying(this.state.token,localStorage.getItem("lastsearch"))
+   // }
+  //  this.loading.pop()
+      
+  
+}
   
 
-  getCurrentlyPlaying(token,query) {
-    // Make a call using the token
-    $.ajax({
-        
-       
-        url: "https://api.spotify.com/v1/artists/"+query+"/albums" ,
-       
-      type: "GET",
-      beforeSend: xhr => {
-        xhr.setRequestHeader("Authorization", "Bearer " + token);
-        xhr.setRequestHeader("Content-Type", " Content-Type ");
-        xhr.setRequestHeader("Accept", "application/json" );
-      },
-      success: data => {
-        
-        this.setState({
-          
-          item: data.items,
-        
-        });
-       console.log("albums",this.state.item)
-      }
-    });
-  }
+  async getCurrentlyPlaying(token,query) {
+  
+   
+    if(this.state.next!==null){
+    try{
+       if(query==""){
+         this.setState({
+             
+           item: [],
+           loading: []
+         }); 
+         return
+       }
+
+       const response = await fetch("https://api.spotify.com/v1/artists/"+query+"/albums?offset="+this.state.offset+"&limit=50",{method:"GET",
+        headers: new Headers ({"Authorization": "Bearer " + token,
+       "Content-Type": " Content-Type ",
+        "Accept": "application/json" 
+       })
+     })
+       const json = await response.json()
+       console.log(json)
+       if(json["error"]){
+         this.props.history.push('/login')
+         
+       }
+       this.setState({
+           offset : this.state.offset + 50,
+         item: this.state.item.concat(...json.items),
+         next: json.next
+       });
+     } catch (error) {
+       console.log(error)
+     }
+    }else{
+      this.setState({
+        loading:[]
+      })
+
+    }
+   }
 
   render() {
       
@@ -63,7 +118,7 @@ class Albums extends React.Component {
            <div style={{margin:"5px"}} >
 
           <Album name={this.props.match.params.name}  list={this.state.item}/>
-   
+          <div style={{textAlign:"center"}}> {this.state.loading}</div>
  
  </div>
 
